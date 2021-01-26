8c2ff38611e06f1e49a043adab9cda5653a12143
Teodor Kanev <thkanev@gmail.com>
2021-01-26 19:04:58 +0200

    added login

---
+++ api/login.php
@@ -0,0 +1,21 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+	require_once('database.php');
+	session_start();
+    $username = $_POST["username"];
+    $password = $_POST['password'];
+	$db = new database();
+	$data = array($username,$password);
+	$result = $db->loginCheck($data);
+	if ($result['success']){
+		$_SESSION['loggedIn'] = true;
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password;
+	}
+	echo json_encode($result);
+
+?>
